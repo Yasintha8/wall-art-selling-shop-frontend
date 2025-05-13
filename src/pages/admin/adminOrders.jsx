d7f0dac5a47fb6cf5d686efed08700c85adbb56c
Yasintha28 <yasinthachamikara28@gmail.com>
2025-05-13 07:50:08 +0530

    update editOrderModel and create change orderStatus✏

--- src/pages/admin/adminOrders.jsx
+++ src/pages/admin/adminOrders.jsx
@@ -2,6 +2,7 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import Loader from "../../components/loader"
 import { IoCloseSharp } from "react-icons/io5"
+import toast from "react-hot-toast"
 
 export function AdminOrdersPage() {
 
@@ -28,6 +29,24 @@ export function AdminOrdersPage() {
         }
     }, [loaded]
 )
+
+    function changeOrderStatus(orderID, status){
+        const token = localStorage.getItem("token");
+        axios.put(import.meta.env.VITE_BACKEND_URL + "/api/order/"+orderID, {
+           status: status
+        }, {
+            headers: {
+                "Authorization": "Bearer "+token
+            }
+        }).then(
+            (response) => {
+                console.log("Order status changed successfully", response.data)
+                toast.success("Order status changed successfully")
+                setLoaded(false)
+            }
+        )
+
+    }
     return (
         <div className="w-full h-full rounded-lg relative">
             {
@@ -44,6 +63,7 @@ export function AdminOrdersPage() {
                                     <th >Status</th>
                                     <th >Total Price</th>
                                     <th >Date</th>
+                                    <th></th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -51,20 +71,35 @@ export function AdminOrdersPage() {
                         orders.map( 
                             (order)=>{
                                 return (
-                                    <tr key={order.orderId} 
-                                    onClick={()=>{
-                                        setModalIsDisplaying(true)
-                                        setDisplayingOrder(order)
-                                    }}
+                                    <tr key={order.orderID} 
                                     className="border-b-2 border-gray-300 text-center hover:bg-gray-100 cursor-pointer">
                                         <td className="p-2">{order.orderID}</td>
                                         <td className="p-2">{order.email}</td>
                                         <td className="p-2">{order.name}</td>
                                         <td className="p-2">{order.address}</td>
                                         <td className="p-2">{order.phoneNumber}</td>
-                                        <td className="p-2">{order.status}</td>
+                                        <td className="p-2">
+                                            <select value={order.status} onChange={
+                                                (e)=>{
+                                                    changeOrderStatus(order.orderID, e.target.value)
+                                                }
+                                            }>
+                                                <option value="Pending">Pending</option>
+                                                <option value="Processing">Processing</option>
+                                                <option value="Shipped">Shipped</option>
+                                                <option value="Delivered">Delivered</option>
+                                                <option value="Cancelled">Cancelled</option>
+                                            </select>
+                                        </td>
                                         <td className="p-2">{order.total.toFixed(2)}</td>
                                         <td className="p-2">{new Date(order.date).toDateString()}</td>
+                                        <td className="p-2">
+                                            <button 
+                                            onClick={()=>{
+                                                setModalIsDisplaying(true)
+                                                setDisplayingOrder(order)
+                                            }}
+                                            className="bg-blue-500 text-white p-2 rounded-lg cursor-pointer">Details</button></td>
                                     </tr>
                                 )
                             }
