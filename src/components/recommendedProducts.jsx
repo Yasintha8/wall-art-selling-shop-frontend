53430a5b82f6b93913b94dfd1497c78f62d4f7cb
Yasintha28 <yasinthachamikara28@gmail.com>
2025-06-25 07:25:54 +0530

    fix: import Link component from react-router-dom in RecommendedProducts

--- src/components/recommendedProducts.jsx
+++ src/components/recommendedProducts.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Loader from "./loader";
+import { Link } from "react-router-dom";
 
 export default function RecommendedProducts() {
   const [products, setProducts] = useState([]);
